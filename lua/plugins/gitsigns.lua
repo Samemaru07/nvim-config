5f3d87a43b7a999a73fb7d444ccba5115568da55
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-07-26 21:41:53 +0900

    feat(plugins): gitsigns.nvim を追加

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,7 @@
+-- 差分がある行の左端に印を表示する
+return {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" }, -- BufReadPre: 既存ファイルを開く直前, BufNewFile: まだ存在しない新規ファイル名で開いたとき
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}
+}
