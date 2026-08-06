209535612e7d6a4abd52e318ffa0d1288a080d90
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-08-06 11:16:13 +0900

    feat(plugins): dropbar.nvimを追加

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,12 @@
+-- 画面上部にカレントバッファのパス階層をパンくず表示する
+return {
+    "Bekaboo/dropbar.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "nvim-telescope/telescope-fzf-native.nvim"
+    },
+    config = function()
+        local dropbar_api = require("dropbar.api")
+        vim.keymap.set("n", "<leader>;", dropbar_api.pick, { desc = "Pick Symbol in Winbar" })
+    end
+}
