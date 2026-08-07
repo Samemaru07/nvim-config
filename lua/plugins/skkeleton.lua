7bfee9f2506579a34146dca2ef60613bd6f0f815
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-08-07 20:44:35 +0900

    fix(plugins): denops.vimのバージョン指定をcommit pinに統一

--- lua/plugins/skkeleton.lua
+++ lua/plugins/skkeleton.lua
@@ -3,7 +3,7 @@ return {
 	commit = "b530eac5a859ce2f8fa4d99fa5cd83b9d3199086",
 	lazy = false,
 	dependencies = {
-		{ "vim-denops/denops.vim", commit = "1df7a022d6e9cb3f6a3db43235e0f174ccd79e03" },
+		"vim-denops/denops.vim",
 	},
 	config = function()
 		vim.fn["skkeleton#config"]({
