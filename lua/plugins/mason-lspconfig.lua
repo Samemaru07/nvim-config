84dd4343c4f1761719cc460752dbc0b9c685f840
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-08-07 10:44:23 +0900

    feat(lsp): clangdをensure_installedに追加

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,21 +1,22 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "neovim/nvim-lspconfig"
-    },
-    opts = {
-        -- サーバ名
-        -- 補完・診断・定義ジャンプ・参照検索・ホバー・リネーム・コードアクションを提供してくれる
-        ensure_installed = {
-            "lua_ls",
-            "pyright",
-            "texlab",
-            "marksman",
-            "html",
-            "cssls",
-            "ts_ls",
-            "bashls"
-        } 
-    }
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	opts = {
+		-- サーバ名
+		-- 補完・診断・定義ジャンプ・参照検索・ホバー・リネーム・コードアクションを提供してくれる
+		ensure_installed = {
+			"lua_ls",
+			"pyright",
+			"clangd",
+			"texlab",
+			"marksman",
+			"html",
+			"cssls",
+			"ts_ls",
+			"bashls",
+		},
+	},
 }
