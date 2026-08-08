fa3e0d15bff17abd332f4a712cb028cdd3ff73f9
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-08-08 19:20:52 +0900

    fix(plugins): pyrightのroot_dirがcwd全体を参照する問題を修正

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,11 +2,11 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = { "saghen/blink.cmp" },
 	config = function()
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
+	local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-		-- 全てのLSPサーバに共通適用
-		vim.lsp.config("*", {
-			capabilities = capabilities,
-		})
+	-- 全てのLSPサーバに共通適用
+	vim.lsp.config("*", {
+		capabilities = capabilities,
+	})
 	end,
 }
