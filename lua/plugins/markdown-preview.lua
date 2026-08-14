718008918a741317ab9789e6d0be5e3923d62e6d
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-08-14 10:42:08 +0900

    fix(plugins): markdown-preview.nvimのbuildフックをnpm installコマンドに変更
    補足: 保存時にconform.nvim (prettier) による全体整形が入っている

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,19 +1,17 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  ft = { "markdown" },
-  cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  init = function()
-    -- デフォルトのブラウザ起動処理を差し替え、Hyperのwebview機能でプレビューURLを開く
-    -- TODO: Hyper側のwebview起動コマンドが確定したら、下記コマンド文字列を実装に合わせて書き換える
-    vim.g.mkdp_browserfunc = "OpenMarkdownPreviewInHyperWebview"
-    vim.cmd([[
+	"iamcco/markdown-preview.nvim",
+	ft = { "markdown" },
+	cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+	build = "cd app && npm install",
+	init = function()
+		-- デフォルトのブラウザ起動処理を差し替え、Hyperのwebview機能でプレビューURLを開く
+		-- TODO: Hyper側のwebview起動コマンドが確定したら、下記コマンド文字列を実装に合わせて書き換える
+		vim.g.mkdp_browserfunc = "OpenMarkdownPreviewInHyperWebview"
+		vim.cmd([[
       function! OpenMarkdownPreviewInHyperWebview(url)
         " 例: Hyper側にURLを渡すコマンドをここに実装する
         " call system('hyper-webview-open ' . shellescape(a:url))
       endfunction
     ]])
-  end,
+	end,
 }
