2c0dc09906507ea0920f5b7475aff00c0acbb8ad
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-09-02 07:48:23 +0900

    feat(kanagawa): 背景を透過表示に設定

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -3,6 +3,9 @@ return {
 	lazy = false, -- 起動時に即読み込み
 	priority = 1000,
 	config = function()
+		require("kanagawa").setup({
+			transparent = true,
+		})
 		vim.cmd.colorscheme("kanagawa")
 	end,
 }
