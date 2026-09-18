89d838d7662e6a8e8919cfa1b688e770c307345d
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-09-18 10:06:15 +0900

    feat(vimtex): indent.logの出力を無効化

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -11,6 +11,7 @@ return {
 		vim.g.vimtex_compiler_progname = "/home/samemaru/.local/bin/nvr"
 		vim.g.vimtex_compiler_method = "latexmk"
 		vim.g.vimtex_compiler_latexmk_engines = { _ = "-lualatex" }
+		vim.g.vimtex_indent_debug = 0
 		vim.g.vimtex_compiler_latexmk = {
 			continuous = 1,
 			out_dir = ".build",
