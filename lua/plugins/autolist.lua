b9cc3c03ad49c14f24cb78b8ae07c0cd89379605
Samemaru07 <189515013+Samemaru07@users.noreply.github.com>
2026-09-26 06:29:47 +0900

    refactor(config): 他プラグインとのキー競合回避およびバッファ設定適用処理の共通化のため、autolistの設定を修正

--- lua/plugins/autolist.lua
+++ lua/plugins/autolist.lua
@@ -10,34 +10,56 @@ return {
 		local autolist = require("autolist")
 		autolist.setup()
 
+		local target_fts = { markdown = true, text = true, tex = true, plaintex = true }
+
+		local function is_empty_marker(line)
+			return line:match("^%s*[%-%*%+]%s*$") ~= nil or line:match("^%s*%d+%.%s*$") ~= nil
+		end
+
+		local function setup_buffer()
+			local opts = { buffer = true }
+
+			vim.opt_local.comments = { "b:-", "b:*", "b:+", "b:1." }
+			vim.opt_local.formatoptions:append("r")
+
+			vim.keymap.set("i", "<Tab>", "<cmd>AutolistTab<cr>", opts)
+			vim.keymap.set("i", "<S-Tab>", "<cmd>AutolistShiftTab<cr>", opts)
+			vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>", opts)
+			vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>", opts)
+			vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>", opts)
+			vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>", opts)
+			vim.keymap.set({ "n", "v" }, ">>", ">><cmd>AutolistRecalculate<cr>", opts)
+			vim.keymap.set({ "n", "v" }, "<<", "<<<cmd>AutolistRecalculate<cr>", opts)
+			vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>", opts)
+			vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>", opts)
+		end
+
+		-- ft指定でのプラグイン読み込み時点で、現在のバッファは既にFileTypeイベントが
+		-- 発火済みのため、以降のautocmdでは捕捉できない。ここで即時適用する。
+		setup_buffer()
+
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = { "markdown", "text", "tex", "plaintex" },
+			callback = setup_buffer,
+		})
+
+		-- 空のリスト行が連続した場合、マーカーを消去して平文の空行に戻す。
+		-- <CR>自体は奪わない（skkeleton/blink.cmp/nvim-autopairsとの競合回避のため）。
+		vim.api.nvim_create_autocmd("TextChangedI", {
 			callback = function()
-				local opts = { buffer = true }
-
-				vim.opt_local.comments = { "b:-", "b:*", "b:+", "b:1." }
-				vim.opt_local.formatoptions:append("r")
-				vim.opt_local.formatoptions:append("o")
-
-				-- 空のリスト行でEnterを押した場合はマーカーを消去して平文に戻す
-				vim.keymap.set("i", "<CR>", function()
-					local line = vim.api.nvim_get_current_line()
-					if line:match("^%s*[%-%*%+]%s*$") or line:match("^%s*%d+%.%s*$") then
-						return "<C-u><CR>"
-					end
-					return "<CR>"
-				end, { buffer = true, expr = true })
-
-				vim.keymap.set("i", "<Tab>", "<cmd>AutolistTab<cr>", opts)
-				vim.keymap.set("i", "<S-Tab>", "<cmd>AutolistShiftTab<cr>", opts)
-				vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>", opts)
-				vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>", opts)
-				vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>", opts)
-				vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>", opts)
-				vim.keymap.set({ "n", "v" }, ">>", ">><cmd>AutolistRecalculate<cr>", opts)
-				vim.keymap.set({ "n", "v" }, "<<", "<<<cmd>AutolistRecalculate<cr>", opts)
-				vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>", opts)
-				vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>", opts)
+				if not target_fts[vim.bo.filetype] then
+					return
+				end
+				local row = vim.api.nvim_win_get_cursor(0)[1]
+				if row < 2 then
+					return
+				end
+				local cur = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1] or ""
+				local prev = vim.api.nvim_buf_get_lines(0, row - 2, row - 1, false)[1] or ""
+				if is_empty_marker(cur) and is_empty_marker(prev) then
+					vim.api.nvim_buf_set_lines(0, row - 2, row, false, { "" })
+					vim.api.nvim_win_set_cursor(0, { row - 1, 0 })
+				end
 			end,
 		})
 	end,
